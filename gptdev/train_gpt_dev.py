babf32e538be783cf7e11f6faf3b148cf3a51262
laytcai <1715782167@qq.com>
2025-12-08 20:34:10 -0500

    fix dev again2

--- gptdev/train_gpt_dev.py
+++ gptdev/train_gpt_dev.py
@@ -319,8 +319,8 @@ class DistributedDataLoader:
 @dataclass
 class Hyperparameters:
     # data hyperparams
-    input_bin : str = 'fineweb10B/fineweb_train_*.bin' # input .bin to train on
-    input_val_bin : str = 'fineweb10B/fineweb_val_*.bin' # input .bin to eval validation loss on
+    input_bin : str = '../fineweb10B/fineweb_train_*.bin' # input .bin to train on
+    input_val_bin : str = '../fineweb10B/fineweb_val_*.bin' # input .bin to eval validation loss on
     # optimization hyperparams
     batch_size : int = 8*64 # batch size, in sequences, across all devices
     device_batch_size : int = 64 # batch size, in sequences, per device
