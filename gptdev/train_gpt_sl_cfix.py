a113340dfc9fb4e5a5006ee5a58e69ef7dbeba0a
laytcai <1715782167@qq.com>
2025-12-09 11:46:22 -0500

    try running compiled stuff

--- gptdev/train_gpt_sl_cfix.py
+++ gptdev/train_gpt_sl_cfix.py
@@ -386,7 +386,7 @@ model = GPT(GPTConfig(vocab_size=num_vocab, n_layer=12, n_head=6, n_embd=768))
 model = model.cuda()
 if hasattr(config, "coordinate_descent_tuning"):
     config.coordinate_descent_tuning = True # suggested by @Chillee
-model = torch.compile(model) # keep default compile; cudagraphs marked per step
+model = torch.compile(model, mode="reduce-overhead") # favor cudagraph capture for steady-state speed
 # here we wrap model into DDP container
 model = DDP(model, device_ids=[ddp_local_rank])
 raw_model = model.module # always contains the "raw" unwrapped model
