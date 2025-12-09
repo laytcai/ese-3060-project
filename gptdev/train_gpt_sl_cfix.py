e14f0d681cd8254fac3a6cdd83b70f5be5a160ee
laytcai <1715782167@qq.com>
2025-12-09 13:18:35 -0500

    try to fix sl again

--- gptdev/train_gpt_sl_cfix.py
+++ gptdev/train_gpt_sl_cfix.py
@@ -111,7 +111,7 @@ class Rotary(torch.nn.Module):
 
     def __init__(self, dim, base=10000):
         super().__init__()
-        self.inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
+        self.register_buffer("inv_freq", 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim)))
         self.seq_len_cached = None
         self.cos_cached = None
         self.sin_cached = None
@@ -120,8 +120,6 @@ class Rotary(torch.nn.Module):
         seq_len = x.shape[1]
         if seq_len != self.seq_len_cached:
             self.seq_len_cached = seq_len
-            if self.inv_freq.device != x.device:
-                self.inv_freq = self.inv_freq.to(x.device)
             t = torch.arange(seq_len, device=x.device, dtype=self.inv_freq.dtype)
             freqs = torch.outer(t, self.inv_freq)
             self.cos_cached = freqs.cos().bfloat16()
